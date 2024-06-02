f4e030671b61037cc13818c802a09fcc671dce01
syedaifrase <2020f-mulbsswe-007@mul.edu.pk>
2024-06-02 20:05:15 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
+
+// Menu show
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Menu hidden
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                        :header.classList.remove('-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== SWIPER FAVORITES ===============*/ 
+const swiperFavorites = new Swiper('.favorites_swiper ', {
+    loop: true,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+ 
+  
+  });
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
+                       : scrollUp.classList.remove('show-scroll')
+                       
+
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionsClass = document.querySelector('.nav_menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+        
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    // reset: true,
+})
+
+sr.reveal('.home_data, .favorites_container , .footer_container')
+sr.reveal('.home_circle, .home_img', {delay: 600, scale:.5} )
+sr.reveal('.home_chips-1, .home_chips-2, .home_chips-3', {delay:1000, interval: 100})
+sr.reveal('.home_leaf', {delay:1200})
+sr.reveal('.home_tomato-1, .home_tomato-2', {delay:1400, interval: 100})
+sr.reveal('.care_img, .contact_img',{origin:'left'})
+sr.reveal('.care_list, .contact_data',{origin:'right'})
+sr.reveal('.banner_item, .products_card', {interval:100})
